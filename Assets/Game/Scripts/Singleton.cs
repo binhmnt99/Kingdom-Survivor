2460904e2db2da5bc6b6decdd857f41c65a03d4a
Mai Nguyen Thanh Binh <76925857+binhmnt99@users.noreply.github.com>
2024-02-29 14:27:18 +0700

    fix actionsystem

--- Assets/Game/Scripts/Singleton.cs
+++ Assets/Game/Scripts/Singleton.cs
@@ -12,7 +12,7 @@ public class Singleton<T> : MonoBehaviour where T : Component
             {
                 if (instance == null)
                 {
-                    instance = FindAnyObjectByType<T>();
+                    instance = FindFirstObjectByType<T>();
                     if (instance == null)
                     {
                         GameObject gameObject = new GameObject();
